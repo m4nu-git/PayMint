be7fffa7ead4542470aa3373a86dac65661c19b1
m4nudev <manudev912@gmail.com>
2024-12-26 23:20:22 +0530

    added more components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,21 @@
-import  {
+import {
   BrowserRouter,
-  Route,
   Routes,
+  Route
 } from "react-router-dom";
-
-import { Signup } from "./pages/Signup"; 
-import { Signin } from "./pages/Signin"; 
-import { Dashboard } from "./pages/Dashboard"; 
-import { SendMoney } from "./pages/SendMoney.jsx"; 
+import { useState } from 'react'
+import { Signup } from "./pages/Signup"
 
 function App() {
-  
+  const [count, setCount] = useState(0)
 
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-    n <Route path="/" element={<Signup />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/send" element={<SendMoney />} />
-    </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,8 +1,8 @@
-export function InputBox ({ label, plceholder, onChange }) {
+export function InputBox({ label, placeholder, onChange }) {
     return <div>
         <div className="text-sm font-medium text-left py-2">
             {label}
         </div>
-        <input onChange={onChange} placeholder={plceholder} className="w-full px-2 py-1 border rounded border-slate-200"></input>
+        <input onChange={onChange} placeholder={placeholder} className="w-full  px-2 py-1 border rounded border-slate-200" />
     </div>
 }
